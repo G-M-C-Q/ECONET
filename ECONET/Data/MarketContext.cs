853583c1503ba13786b964b2d8036df7a2439f08
Gillian Mc Quillan <X00179268@myTUDublin.ie>
2022-08-14 16:13:45 +0100

    Change from SQLITE to SQLServer

--- ECONET/Data/MarketContext.cs
+++ ECONET/Data/MarketContext.cs
@@ -3,17 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ECONET.Data
 {
-    public class MarketContext :DbContext
+    public class MarketContext : DbContext
 
     {
-
+        
         public MarketContext(DbContextOptions<MarketContext> options) : base(options)
         {
         }
-            //properties DBSet 
-            //access items and use methods in DB context to find an entity with ID
-            public DbSet<Item> Items { get; set; }
-        }
-
-
+        //properties DBSet 
+        //access items and use methods in DB context to find an entity with ID
+        //items tabled generated in database
+        public DbSet<Item> Items { get; set; }
     }
+
+
+ }
--- ECONET/Program.cs
+++ ECONET/Program.cs
@@ -4,23 +4,24 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 
+
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<MarketContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString") ?? throw new InvalidOperationException("Connection string not found.")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-builder.Services.AddDbContext <MarketContext>(x => x.UseSqlite(IConfiguration.GetConnectionString));
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+} 
 
 app.UseHttpsRedirection();
 
