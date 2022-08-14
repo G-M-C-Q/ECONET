dde06a719ecacfd2f4cb06f223f25d03beca0379
Gillian Mc Quillan <X00179268@myTUDublin.ie>
2022-08-14 18:45:04 +0100

    Seed test for items

---
+++ ECONET/Data/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using ECONET.Entities;
+
+namespace ECONET.Data
+{
+    public class DbInitializer
+    {
+
+
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+
+                var context = serviceScope.ServiceProvider.GetService<MarketContext>();
+
+                context.Database.EnsureCreated();
+                //Cinema
+                if (!context.Items.Any())
+                {
+                    context.Items.AddRange(new List<Item>()
+                    {
+                        new Item()
+                        {
+                            Id = 1,
+                            Name = "T-shirt",
+                        },
+                    
+                    });
+                    context.SaveChanges();
+                   
+
+                }
+            }
+
+        }
+    }
+}
