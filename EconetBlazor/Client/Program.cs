c36d7cde00029b01d4dab6b0462d6cfb51b6a017
Gillian Mc Quillan <X00179268@myTUDublin.ie>
2022-11-13 22:21:00 +0000

    Logout option when logged in

--- EconetBlazor/Client/Program.cs
+++ EconetBlazor/Client/Program.cs
@@ -2,12 +2,14 @@ global using EconetBlazor.Shared;
 global using System.Net.Http.Json;
 global using EconetBlazor.Client.Services.ProductService;
 global using EconetBlazor.Client.Services.CategoryService;
+global using EconetBlazor.Client.Services.AuthService;
+global using Microsoft.AspNetCore.Components.Authorization;
 using EconetBlazor.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
 using EconetBlazor.Client.Services.CartService;
-using EconetBlazor.Client.Services.AuthService;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -22,5 +24,8 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 await builder.Build().RunAsync();
