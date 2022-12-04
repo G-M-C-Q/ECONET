15ac7c149f3a818c39cf7307c972edc1376f658a
Gillian Mc Quillan <X00179268@myTUDublin.ie>
2022-12-04 17:34:04 +0000

    Admin product crud services

--- EconetBlazor/Shared/Product.cs
+++ EconetBlazor/Shared/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ namespace EconetBlazor.Shared
     public class Product
     {
         public int Id { get; set; }
-    
+        [Required]
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
@@ -20,7 +21,12 @@ namespace EconetBlazor.Shared
         public Category? Category { get; set; }
         public int CategoryId { get; set; }
         public bool Featured { get; set; } = false;
-
+        public bool Visible { get; set; } = true;
+        public bool Deleted { get; set; } = false;
+        [NotMapped]
+        public bool Editing { get; set; } = false;
+        [NotMapped]
+        public bool IsNew { get; set; } = false;
 
     }
 }
